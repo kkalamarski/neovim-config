1999eafeeec5cb15ecec5175d378bf66eb159f8d
Krzysztof Kalamarski <k.kalamarski@exlabs.pl>
2021-04-30 14:30:31 -0300

    Terminal & Statusline tewaks

--- lua/plugins/feline.lua
+++ lua/plugins/feline.lua
@@ -1,4 +1,4 @@
 require('feline').setup({
   preset = 'noicon',
-  default_bg = '#1A1B2A'
+  default_bg = '#0e0e16'
 })
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -5,8 +5,28 @@ require"toggleterm".setup{
   shade_filetypes = {},
   shade_terminals = true,
   shading_factor = '1', -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
-  start_in_insert = false,
+  start_in_insert = true,
   persist_size = true,
   direction = 'horizontal',
   shell = vim.o.shell -- change the default shell 
 }
+
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ 
+  cmd = "lazygit", 
+  hidden = true,
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  on_open = function(term)
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "<ESC>", "<cmd>close<CR>", {noremap = true, silent = true})
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+})
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", "<leader>g", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
